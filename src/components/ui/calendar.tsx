94c9a7a783e1a058b81d2aa0ec66936718860eed
josuerx12 <josueaze12@gmail.com>
2025-05-08 01:36:48 -0300

    :hammer:refact: Ajustes no package json do react day picker.

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -60,11 +60,11 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
+        PreviousMonthButton: ({ className, ...props }) => (
+          <ChevronLeft className={cn("h-4 w-4", className)} />
         ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
+        NextMonthButton: ({ className, ...props }) => (
+          <ChevronRight className={cn("h-4 w-4", className)} />
         ),
       }}
       {...props}
