62dd5a904464dd4e5a9517080c80b728b170e713
josuerx12 <josueaze12@gmail.com>
2025-07-07 15:21:17 -0300

    :hammer:refact: Ajuste no codigo.

--- src/app/registre-se/page.tsx
+++ src/app/registre-se/page.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Image from "next/image";
 import SignUpForm from "./form";
 
 const Page = () => {
   return (
     <div className="w-full h-full md:h-screen flex justify-center py-10 px-4">
       <div className="flex flex-col gap-6 p-6 py-8 h-fit max-w-md w-full bg-white shadow-lg rounded-2xl border border-gray-200">
-        {/* Logo e Mensagem de Boas-vindas */}
         <div className="flex flex-col items-center">
           <img src="/logo.svg" width={80} height={80} alt="Logo Floricultura" />
           <h2 className="mt-3 text-lg font-semibold text-gray-700 text-center">
@@ -18,7 +16,6 @@ const Page = () => {
           </p>
         </div>
 
-        {/* Formulário de Cadastro */}
         <SignUpForm />
       </div>
     </div>
